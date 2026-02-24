7353c543c0317188bd4774584a58d43b8bdc3702
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2026-02-24 18:06:54 -0300

    feat: Inicializando o DateFormatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:pocket_vault/data/database_helper.dart';
 import 'package:pocket_vault/enums/theme_mode_enum.dart';
 import 'package:pocket_vault/providers/user_preferences_provider.dart';
-import 'package:pocket_vault/screens/home_screen.dart';
+import 'package:pocket_vault/screens/home/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await initializeDateFormatting();
   await DatabaseHelper.instance.database;
 
   runApp(const ProviderScope(child: MyApp()));
