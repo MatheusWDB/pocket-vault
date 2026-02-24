a48822111659ff08417c74f057e0bfdde4ed9572
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2026-02-24 17:55:02 -0300

    feat: Acrescentado locale e decimalDigits

--- lib/enums/currency_symbol_enum.dart
+++ lib/enums/currency_symbol_enum.dart
@@ -1,13 +1,21 @@
 enum CurrencySymbolEnum {
-  brl('BRL', 'R\$', 'Português (Brasil)'),
-  usd('USD', '\$', 'English (USA)'),
-  eur('EUR', '€', 'Euro (Europa)'),
-  gbp('GBP', '£', 'Pound (UK)'),
-  jpy('JPY', '¥', 'Yen (Japan)');
+  brl('BRL', 'R\$', 'pt_BR', 'Português (Brasil)', 2),
+  usd('USD', '\$', 'en_US', 'English (USA)', 2),
+  eur('EUR', '€', 'de_DE', 'Euro (Europa)', 2),
+  gbp('GBP', '£', 'en_GB', 'Pound (UK)', 2),
+  jpy('JPY', '¥', 'ja_JP', 'Yen (Japan)', 0);
 
   final String code;
   final String symbol;
+  final String locale;
   final String displayName;
+  final int decimalDigits;
 
-  const CurrencySymbolEnum(this.code, this.symbol, this.displayName);
+  const CurrencySymbolEnum(
+    this.code,
+    this.symbol,
+    this.locale,
+    this.displayName,
+    this.decimalDigits,
+  );
 }
